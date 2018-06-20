983b9a5b01b82ba11d39cb739df1cfc62016bfaf
Jennifer Marriott <jennifer.marriott@telus.com>
2018-06-20 21:49:34 +0000

    Thursday

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,26 @@
 get '/' do
-    
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
+end
+
+get '/signup' do        # if a user nvaigates to the path "/signup",
+    @user = User.new    # setup empty @user object
+    erb(:signup)        # render "app/views/signup.erb"
+end
+
+post '/signup' do
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+    
+    # instantiate and save a User
+    @user = User.new({ email: email, avatar_url:avatar_url, username: username, password: password})
+    
+    # if user validations pass and user is saved
+    if @user.save
+        "User #{username} saved!"
+    else
+        erb(:signup)
+    end
 end
